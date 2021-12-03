e85ae3e6c6f479d5bde8d489db785507624feea8
Peter van der Heijden <peter@vanderheijden.name>
2021-12-03 07:14:45 +0100

    Refactor.

--- day03/lib.py
+++ day03/lib.py
@@ -1,14 +1,17 @@
 def part1(rows):
-    binary = ""
-    for index in range(len(rows[0])):
-        binary += get_bit(rows, index)
-
+    binary = "".join(get_bit(rows, index) for index in range(len(rows[0])))
     epsilon = int(binary, base=2)
     gamma = (1 << len(binary)) - epsilon - 1
-
     return epsilon * gamma
 
-def get_bit(rows, index, most_common = True):
+
+def part2(rows):
+    oxygen = select_rating(rows, True)
+    co2 = select_rating(rows, False)
+    return oxygen * co2
+
+
+def get_bit(rows, index, most_common=True):
     length = len(rows)
     column = list(zip(*rows))[index]
     bits_set = sum(int(bit) for bit in column)
@@ -17,22 +20,11 @@ def get_bit(rows, index, most_common = True):
     return "0" if bits_set * 2 >= length else "1"
 
 
-def part2(rows):
-    oxygen_rows = list(rows)
+def select_rating(rows, most_common):
+    selected_rows = list(rows)
     index = 0
-    while len(oxygen_rows) > 1:
-        bit = get_bit(oxygen_rows, index, True)
-        oxygen_rows = [row for row in oxygen_rows if row[index] == bit]
+    while len(selected_rows) > 1:
+        bit = get_bit(selected_rows, index, most_common)
+        selected_rows = [row for row in selected_rows if row[index] == bit]
         index += 1
-    
-    co2_rows = list(rows)
-    index = 0
-    while len(co2_rows) > 1:
-        bit = get_bit(co2_rows, index, False)
-        co2_rows = [row for row in co2_rows if row[index] == bit]
-        index += 1
-    
-    oxygen = int(oxygen_rows[0], base=2)
-    co2 = int(co2_rows[0], base=2)
-
-    return oxygen * co2
+    return int(selected_rows[0], base=2)
