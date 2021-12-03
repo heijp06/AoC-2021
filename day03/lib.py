064f73a065fa4f4593910497218ffebe2b956064
Peter van der Heijden <peter@vanderheijden.name>
2021-12-03 07:10:00 +0100

    Solve day 3 part 2.

--- day03/lib.py
+++ day03/lib.py
@@ -1,14 +1,38 @@
 def part1(rows):
-    length = len(rows)
     binary = ""
-    for column in zip(*rows):
-        bits_set = sum(int(bit) for bit in column)
-        binary += "1" if bits_set > length // 2 else "0"
+    for index in range(len(rows[0])):
+        binary += get_bit(rows, index)
+
     epsilon = int(binary, base=2)
     gamma = (1 << len(binary)) - epsilon - 1
 
     return epsilon * gamma
 
+def get_bit(rows, index, most_common = True):
+    length = len(rows)
+    column = list(zip(*rows))[index]
+    bits_set = sum(int(bit) for bit in column)
+    if most_common:
+        return "1" if bits_set * 2 >= length else "0"
+    return "0" if bits_set * 2 >= length else "1"
+
 
 def part2(rows):
-    pass
+    oxygen_rows = list(rows)
+    index = 0
+    while len(oxygen_rows) > 1:
+        bit = get_bit(oxygen_rows, index, True)
+        oxygen_rows = [row for row in oxygen_rows if row[index] == bit]
+        index += 1
+    
+    co2_rows = list(rows)
+    index = 0
+    while len(co2_rows) > 1:
+        bit = get_bit(co2_rows, index, False)
+        co2_rows = [row for row in co2_rows if row[index] == bit]
+        index += 1
+    
+    oxygen = int(oxygen_rows[0], base=2)
+    co2 = int(co2_rows[0], base=2)
+
+    return oxygen * co2
--- day03/test_lib.py
+++ day03/test_lib.py
@@ -2,7 +2,7 @@ import pytest
 from lib import part1, part2
 
 
-def test_lib():
+def test_part1():
     assert part1([
         "00100",
         "11110",
@@ -17,3 +17,20 @@ def test_lib():
         "00010",
         "01010"
     ]) == 198
+
+
+def test_part2():
+    assert part2([
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010"
+    ]) == 230
