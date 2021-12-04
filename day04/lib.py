8726b7d8bf553df0c75890a245c0a8a9d3f63ba0
Peter van der Heijden <peter@vanderheijden.name>
2021-12-04 10:59:31 +0100

    Write boards as text files.

--- day04/lib.py
+++ day04/lib.py
@@ -1,4 +1,5 @@
 from board import Board
+import sys
 
 
 def part1(rows):
@@ -11,41 +12,59 @@ def part1(rows):
             return winner.sum_of_remaining() * number
     return None
 
-def print_boards(org_boards, boards):
-    if len(org_boards) != 100:
-        return
-    for row in range(8):
-        if row:
-            print()
-        for board_row in range(5):
-            for column in range(13):
-                index = row * 10 + column
-                if index >= 100:
-                    continue
-                if column:
-                    print("  ", end='')
-                board = org_boards[index]
-                if board in boards:
-                    print_row(board.rows[board_row])
-                else:
-                    print_row([" "] * 5)
-            print()
-
-def print_row(row):
-    print(f"{row[0]:>2} {row[1]:>2} {row[2]:>2} {row[3]:>2} {row[4]:>2}", end='')
 
 def part2(rows):
     numbers, boards = parse_input(rows)
+    img = 0
     org_boards = list(boards)
-    print_boards(org_boards, boards)
+    print_boards(org_boards, boards, img)
+    img += 1
     for number in numbers:
         for board in boards:
             board.mark(number)
+        print_boards(org_boards, boards, img)
+        img += 1
         new_boards = [board for board in boards if not board.is_winner()]
         if not new_boards:
             return boards[0].sum_of_remaining() * number
         boards = new_boards
     return None
+
+
+def print_boards(org_boards, boards, img):
+    stdout = sys.stdout
+    with open(f"img/img{img:03d}.txt", "w") as f:
+        sys.stdout = f
+        if len(org_boards) != 100:
+            return
+        rows = 8
+        columns = 13
+        for row in range(rows):
+            if row:
+                print()
+            for board_row in range(5):
+                for column in range(columns):
+                    index = row * columns + column
+                    if index >= 100:
+                        continue
+                    if column:
+                        print("  ", end='')
+                    board = org_boards[index]
+                    if board in boards:
+                        print_row(board.rows[board_row])
+                    else:
+                        print_row([None] * 5)
+                print()
+    sys.stdout = stdout
+
+
+def print_row(row):
+    show = [
+        " " if number is None else number
+        for number
+        in row
+    ]
+    print(f"{show[0]:>2} {show[1]:>2} {show[2]:>2} {show[3]:>2} {show[4]:>2}", end='')
 
 
 def parse_input(rows):
