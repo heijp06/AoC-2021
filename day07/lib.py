dd45ff4702f86bac9486acc909d37d5a361b44da
Peter van der Heijden <peter@vanderheijden.name>
2021-12-07 07:46:39 +0100

    Refactor.

--- day07/lib.py
+++ day07/lib.py
@@ -2,33 +2,22 @@ from collections import Counter
 
 
 def part1(data):
-    end = max(data)
-    crabs = [0] * (end + 1)
-    cost = 0
-    for crab in data:
-        crabs[crab] += 1
-        cost += crab
-    min_cost = cost
-    crabs_left = 0
-    crabs_right = sum(crabs) - crabs[0]
-    for pos in range(1, end + 1):
-        cost += crabs_left + crabs[pos - 1] - crabs_right
-        if cost < min_cost:
-            min_cost = cost
-        crabs_left += crabs[pos - 1]
-        crabs_right -= crabs[pos]
-    return min_cost
+    return go(data, lambda distance: distance)
 
 
 def part2(data):
-    counts = dict(Counter(data))
+    return go(data, lambda distance: distance * (distance + 1) // 2)
+
+
+def go(data, cost_func):
+    counts = Counter(data)
     end = max(data)
     min_cost = None
     for pos in range(end + 1):
         cost = 0
         for crab_pos, count in counts.items():
             distance = abs(crab_pos - pos)
-            cost += count * distance * (distance + 1) // 2
+            cost += count * cost_func(distance)
         if min_cost is None or cost < min_cost:
             min_cost = cost
     return min_cost
