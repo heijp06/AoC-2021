8a86ae26c111f16cf651131aff300dd67129ba9d
Peter van der Heijden <peter@vanderheijden.name>
2021-12-09 21:41:48 +0100

    Draw image with pillow.

--- day05/display.py
+++ day05/display.py
@@ -1,4 +1,5 @@
 import sys
+from PIL import Image, ImageDraw, ImageFont
 
 
 class Display:
@@ -31,9 +32,16 @@ class Display:
             print("".join(line))
 
     def write_image(self):
-        stdout = sys.stdout
-        with open(f"img/img{self.image_number:08d}.txt", "w") as f:
-            sys.stdout = f
-            self.show()
-        sys.stdout = stdout
+        font = ImageFont.truetype(r"C:\Windows\Fonts\consola.ttf", 24)
+        (width, height) = font.getsize("X")
+        margin = 10
+        image = Image.new("RGB", (margin * 2 + self.width * width, margin * 2 + self.height * height))
+        draw = ImageDraw.Draw(image)
+        color_table = { ".": 0xff0000, "1": 0xff5555, "2": 0xffaaaa, "3": 0xffffff}
+        for y in range(self.height):
+            for x in range(self.width):
+                char = self.lines[y][x]
+                color = color_table[char]
+                draw.text((margin + width * x, margin + height * y), self.lines[y][x], fill=color, font=font)
+        image.save(f"img/img{self.image_number:08d}.png")
         self.image_number += 1
