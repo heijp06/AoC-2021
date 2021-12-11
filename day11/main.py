986235b96989140d0cfcb17cfbfa45d31d830137
Peter van der Heijden <peter@vanderheijden.name>
2021-12-11 09:40:53 +0100

    Refactor.

--- day11/main.py
+++ day11/main.py
@@ -1,12 +1,9 @@
-import csv
 import pyperclip
 from lib import part1, part2
 
 
 def read_rows(**kwargs):
     with open('data.txt', newline='') as csv_file:
-        # return list(csv.reader(csv_file, **kwargs))
-        # return csv_file.read().strip()
         return csv_file.read().splitlines()
 
 
@@ -24,19 +21,3 @@ clip(x)
 x = part2(rows)
 print(f"Part 2: {x}")
 clip(x)
-
-# data = [
-#     "5483143223",
-#     "2745854711",
-#     "5264556173",
-#     "6141336146",
-#     "6357385478",
-#     "4167524645",
-#     "2176841721",
-#     "6882881134",
-#     "4846848554",
-#     "5283751526"
-# ]
-
-
-# part1(data, 2)
