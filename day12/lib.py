3620e0f0c3a0e189bf23a55b2fa57d4c1edfd03f
Peter van der Heijden <peter@vanderheijden.name>
2021-12-12 08:17:56 +0100

    Refactor.

--- day12/lib.py
+++ day12/lib.py
@@ -2,65 +2,42 @@ from collections import defaultdict
 
 
 def part1(rows: list[str]) -> int:
+    return part(rows, 1)
+
+
+def part2(rows: list[str]) -> int:
+    return part(rows, 2)
+
+
+def part(rows: list[str], number: int) -> int:
     connections = defaultdict(list)
     for row in rows:
         cave1, cave2 = row.split("-")
         connections[cave1].append(cave2)
         connections[cave2].append(cave1)
-    routes = [("start", {"start"})]
+    twice = set() if number == 1 else {
+        cave
+        for cave in connections
+        if cave.islower() and cave not in ["start", "end"]
+    }
+    routes = [
+        ("start", {"start"}, twice)
+    ]
     count = 0
     while routes:
         new_routes = []
-        for (current, visited) in routes:
+        for current, visited, twice in routes:
             for cave in connections[current]:
                 if cave == "end":
                     count += 1
                 elif cave.isupper():
-                    new_routes.append((cave, visited.copy()))
-                elif cave not in visited:
+                    new_routes.append((cave, visited, twice))
+                elif cave in visited:
+                    if cave in twice:
+                        new_routes.append((cave, visited, set()))
+                else:
                     new_visited = visited.copy()
                     new_visited.add(cave)
-                    new_routes.append((cave, new_visited))
-        routes = new_routes
-    return count
-
-
-def part2(rows: list[str]) -> int:
-    connections = defaultdict(list)
-    for row in rows:
-        cave1, cave2 = row.split("-")
-        connections[cave1].append(cave2)
-        connections[cave2].append(cave1)
-    routes = [
-        ("start", {"start"}, small, [])
-        for small in (
-            cave
-            for cave in connections
-            if cave.islower() and cave not in ["start", "end"]
-        )
-    ]
-    count = 0
-    known_routes = set()
-    while routes:
-        new_routes = []
-        for (current, visited, twice, route) in routes:
-            for cave in connections[current]:
-                new_route = route.copy()
-                new_route.append(current)
-                if cave == "end":
-                    path = ",".join(new_route)
-                    if path not in known_routes:
-                        known_routes.add(path)
-                        # print(path, ",end", sep="")
-                        count += 1
-                elif cave.isupper():
-                    new_routes.append((cave, visited.copy(), twice, new_route))
-                elif cave not in visited:
-                    new_visited = visited.copy()
-                    if cave == twice:
-                        new_routes.append((cave, new_visited, "", new_route))
-                    else:
-                        new_visited.add(cave)
-                        new_routes.append((cave, new_visited, twice, new_route))
+                    new_routes.append((cave, new_visited, twice))
         routes = new_routes
     return count
