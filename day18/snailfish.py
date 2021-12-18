da607e4cfc2148b739fc0b8f674f1f901b374e78
Peter van der Heijden <peter@vanderheijden.name>
2021-12-18 10:13:50 +0100

    Implement explode().

--- day18/snailfish.py
+++ day18/snailfish.py
@@ -3,6 +3,11 @@ from abc import ABC, abstractmethod
 import re
 
 
+def parse(row: str) -> Snailfish:
+    parser = Parser(row)
+    return parser.parse()
+
+
 class Parser():
     def __init__(self, row: str) -> None:
         self.row = row
@@ -11,18 +16,13 @@ class Parser():
     def parse(self) -> Snailfish:
         match self.match(r"\[|\d+"):
             case "[":
-                self.index -= 1
-                return self.parse_pair()
+                left = self.parse()
+                self.index += 1
+                right = self.parse()
+                self.index += 1
+                return PairSnailfish(left, right)
             case number:
                 return ValueSnailfish(int(number))
-
-    def parse_pair(self) -> PairSnailfish:
-        self.index += 1
-        left = self.parse()
-        self.index += 1
-        right = self.parse()
-        self.index += 1
-        return PairSnailfish(left, right)
 
     def match(self, pattern: str) -> str:
         result = re.match(pattern, self.row[self.index:])[0]
@@ -34,9 +34,51 @@ class Parser():
 
 
 class Snailfish(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self._parent = None
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
+
     @abstractmethod
     def magnitude(self) -> int:
         pass
+
+    @abstractmethod
+    def explode(self, depth=0) -> bool:
+        pass
+
+    @property
+    def parent(self) -> PairSnailfish:
+        return self._parent
+
+    def first_left(self) -> ValueSnailfish:
+        current = self
+        if not current.parent:
+            return None
+        while current == current.parent.left:
+            current = current.parent
+            if not current.parent:
+                return None
+        current = current.parent.left
+        while not isinstance(current, ValueSnailfish):
+            current = current.right
+        return current
+
+    def first_right(self) -> ValueSnailfish:
+        current = self
+        if not current.parent:
+            return None
+        while current == current.parent.right:
+            current = current.parent
+            if not current.parent:
+                return None
+        current = current.parent.right
+        while not isinstance(current, ValueSnailfish):
+            current = current.left
+        return current
 
 
 class ValueSnailfish(Snailfish):
@@ -50,15 +92,35 @@ class ValueSnailfish(Snailfish):
     def magnitude(self) -> int:
         return self.number
 
+    def explode(self, _=0) -> bool:
+        return False
+
 
 class PairSnailfish(Snailfish):
     def __init__(self, left: Snailfish, right: Snailfish) -> None:
         super().__init__()
         self.left = left
         self.right = right
-    
+        left._parent = self
+        right._parent = self
+
     def __repr__(self) -> str:
         return f'[{self.left},{self.right}]'
-    
+
     def magnitude(self) -> int:
         return 3 * self.left.magnitude() + 2 * self.right.magnitude()
+
+    def explode(self, depth=0) -> bool:
+        if depth < 4:
+            return self.left.explode(depth + 1) or self.right.explode(depth + 1)
+        first_left = self.first_left()
+        if first_left:
+            first_left.number += self.left.number
+        first_right = self.first_right()
+        if first_right:
+            first_right.number += self.right.number
+        if self.parent.left == self:
+            self.parent.left = ValueSnailfish(0)
+        else:
+            self.parent.right = ValueSnailfish(0)
+        return True
--- day18/test_lib.py
+++ day18/test_lib.py
@@ -1,6 +1,6 @@
 import pytest
 from lib import part1, part2
-from snailfish import Parser
+from snailfish import Parser, Snailfish, parse
 
 
 @pytest.mark.parametrize(["row", "expected"], (
@@ -14,6 +14,51 @@ from snailfish import Parser
     ("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488),
 ))
 def test_magnitude(row, expected):
-    parser = Parser(row)
-    snailfish = parser.parse()
+    snailfish = parse(row)
     assert snailfish.magnitude() == expected
+
+
+@pytest.mark.parametrize(["row", "result", "expected"], (
+    ("1", False, "1"),
+    ("[1,2]", False, "[1,2]"),
+    ("[[[[[9,8],1],2],3],4]", True, "[[[[0,9],2],3],4]"),
+    ("[7,[6,[5,[4,[3,2]]]]]", True, "[7,[6,[5,[7,0]]]]"),
+    ("[[6,[5,[4,[3,2]]]],1]", True, "[[6,[5,[7,0]]],3]"),
+    ("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]",
+     True, "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
+    ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", True, "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
+))
+def test_explode(row, result, expected):
+    snailfish = parse(row)
+    changed = snailfish.explode()
+
+    assert changed == result
+    assert str(snailfish) == expected
+
+
+@pytest.mark.parametrize(["row", "first_left", "first_right"], (
+    ("[1,2]", "1", "2"),
+    ("[1,[2,3]]", "1", "2"),
+    ("[[1,2],3]", "2", "3"),
+    ("[[[[1,2],3],4],5]", "4", "5"),
+    ("[1,[2,[3,[4,5]]]]", "1", "2"),
+    ("[[1,[2,[3,4]]],5]", "4", "5"),
+    ("[1,[[[2,3],4],5]]", "1", "2"),
+))
+def test_first_left_and_first_right(row, first_left, first_right):
+    snailfish = parse(row)
+
+    assert str(snailfish.left.first_right()) == first_right
+    assert str(snailfish.left.first_left()) == "None"
+    assert str(snailfish.right.first_right()) == "None"
+    assert str(snailfish.right.first_left()) == first_left
+
+
+def test_parent():
+    snailfish = parse("1")
+    assert snailfish.parent is None
+
+    snailfish = parse("[1,2]")
+    assert snailfish.parent is None
+    assert snailfish.left.parent == snailfish
+    assert snailfish.right.parent == snailfish
