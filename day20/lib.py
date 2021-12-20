c1bb1d069f9b48c63166c4a32c90edd57ca8151f
Peter van der Heijden <peter@vanderheijden.name>
2021-12-20 10:42:42 +0100

    Solve day 20 part 1.

--- day20/lib.py
+++ day20/lib.py
@@ -1,16 +1,24 @@
 def part1(data: list[str]) -> int:
     key = data[0]
+    inverting = False
+    if key[0] == '#':
+        if key[-1] == '#':
+            raise NotImplementedError()
+        inverting = True
     grid = get_grid(data[2:])
-    dump(grid)
-    for _ in range(2):
+    # dump(grid)
+    active_region = set()
+    for step in range(2):
         new_grid = set()
-        active_region = get_active_region(grid)
+        old_active_region = active_region
+        active_region = get_active_region(get_active_region(grid))
         for (x, y) in active_region:
-            index = get_index(grid, x, y)
+            index = get_index(
+                grid, x, y, inverting and step % 2, old_active_region)
             if key[index] == '#':
                 new_grid.add((x, y))
         grid = new_grid
-        dump(grid)
+        # dump(grid)
     return len(grid)
 
 
@@ -48,12 +56,16 @@ def get_active_region(grid: set[tuple[int, int]]) -> set[tuple[int, int]]:
     }
 
 
-def get_index(grid: set[tuple[int, int]], x: int, y: int) -> int:
-    index = 0
+def get_index(grid: set[tuple[int, int]], x: int, y: int, inverting: bool, old_active_region: set[tuple[int, int]]) -> int:
+    index = 511 if inverting else 0
     bit = 256
     for dy in [-1, 0, 1]:
         for dx in [-1, 0, 1]:
-            if (x + dx, y + dy) in grid:
+            pos = (x + dx, y + dy)
+            if inverting:
+                if pos in old_active_region and pos not in grid:
+                    index -= bit
+            elif pos in grid:
                 index += bit
             bit //= 2
     return index
