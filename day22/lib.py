a309f409455333f4d6d7a4ee82dabe2fc11b91a1
Peter van der Heijden <peter@vanderheijden.name>
2021-12-22 15:27:56 +0100

    Refactor.

--- day22/lib.py
+++ day22/lib.py
@@ -3,26 +3,19 @@ import re
 
 
 def part1(rows: list[str]) -> int:
-    blocks = []
-    for row in rows:
-        action, (x0, x1), (y0, y1), (z0, z1) = parse(row)
-        if x0 < -50 or x0 > 50:
-            break
-        block = Block((x0, x1), (y0, y1), (z0, z1))
-        blocks = [
-            rest
-            for existing in blocks
-            for rest in existing - block
-        ]
-        if action == "on":
-            blocks.append(block)
-    return sum(block.size() for block in blocks)
+    return go(rows, 1)
 
 
 def part2(rows):
+    return go(rows, 2)
+
+
+def go(rows: list[str], part: int):
     blocks = []
     for row in rows:
         action, (x0, x1), (y0, y1), (z0, z1) = parse(row)
+        if part == 1 and (x0 < -50 or x0 > 50):
+            break
         block = Block((x0, x1), (y0, y1), (z0, z1))
         blocks = [
             rest
