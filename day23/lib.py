aacff3c3278208a9845c5e949782b6e076802c22
Peter van der Heijden <peter@vanderheijden.name>
2021-12-29 20:41:41 +0100

    Make sure the lowest cost burrows are active.

--- day23/lib.py
+++ day23/lib.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
 from burrow import Burrow, parse
+
+GROUP_SIZE = 1
 
 
 def part1(rows: tuple[str]) -> int:
@@ -14,20 +17,26 @@ def part2(rows: tuple[str]) -> int:
 
 
 def go(rows: list[str]) -> int:
-    burrows = {parse(rows)}
+    groups = defaultdict(set)
+    group = GROUP_SIZE
+    groups[group] = {parse(rows)}
     min_cost = None
-    while burrows:
-        # dump(list(burrows)[0])
-        print(len(burrows), min_cost)
-        new_burrows = set()
+    while (not min_cost or min_cost > group - GROUP_SIZE) and groups:
+        # print(len(burrows), min_cost)
+        # print(group)
+        burrows = groups[group]
+        del groups[group]
         for burrow in burrows:
             for new_burrow in burrow.move():
                 if not min_cost or new_burrow.cost < min_cost:
                     if new_burrow.final():
                         min_cost = new_burrow.cost
                     else:
-                        new_burrows.add(new_burrow)
-        burrows = new_burrows
+                        new_group = GROUP_SIZE + new_burrow.cost // GROUP_SIZE * GROUP_SIZE
+                        groups[new_group].add(new_burrow)
+        if not groups[group]:
+            del groups[group]
+            group += GROUP_SIZE
     return min_cost
 
 
