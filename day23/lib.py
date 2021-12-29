5dada6d3ea78cb14165d945a43f8843a89ae8fdc
Peter van der Heijden <peter@vanderheijden.name>
2021-12-29 21:44:43 +0100

    Use a SortedDict to make sure always the lowest cost burrows are moved.

--- day23/lib.py
+++ day23/lib.py
@@ -1,7 +1,5 @@
-from collections import defaultdict
 from burrow import Burrow, parse
-
-GROUP_SIZE = 1
+from sortedcontainers import SortedDict
 
 
 def part1(rows: tuple[str]) -> int:
@@ -17,26 +15,23 @@ def part2(rows: tuple[str]) -> int:
 
 
 def go(rows: list[str]) -> int:
-    groups = defaultdict(set)
-    group = GROUP_SIZE
-    groups[group] = {parse(rows)}
+    groups = SortedDict()
+    burrows = {parse(rows)}
     min_cost = None
-    while (not min_cost or min_cost > group - GROUP_SIZE) and groups:
-        # print(len(burrows), min_cost)
-        # print(group)
-        burrows = groups[group]
-        del groups[group]
+    cost = 0
+    while not min_cost or cost < min_cost:
         for burrow in burrows:
             for new_burrow in burrow.move():
                 if not min_cost or new_burrow.cost < min_cost:
                     if new_burrow.final():
                         min_cost = new_burrow.cost
+                    elif new_burrow.cost in groups:
+                        groups[new_burrow.cost].add(new_burrow)
                     else:
-                        new_group = GROUP_SIZE + new_burrow.cost // GROUP_SIZE * GROUP_SIZE
-                        groups[new_group].add(new_burrow)
-        if not groups[group]:
-            del groups[group]
-            group += GROUP_SIZE
+                        groups[new_burrow.cost] = {new_burrow}
+        if not groups:
+            break
+        cost, burrows = groups.popitem(index=0)
     return min_cost
 
 
